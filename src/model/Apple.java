080623e6da1aa3d246a2a3dd34a749b9af791e77
WalterTheFox <walterthefox1337@gmail.com>
2025-08-08 20:26:03 +0300

    fix 2

--- src/model/Apple.java
+++ src/model/Apple.java
@@ -1,9 +1,9 @@
 package model;
 
-import model.constants.Colour;
+import  model.constants.Colour;
 import model.constants.Discount;
 
-public class Apple extends Food implements Discountable {
+public class Apple extends Food {
     private String colour;
 
     public Apple(int amount, double price, String colour) {
